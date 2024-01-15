2d028f017837c93c155f0871351a73fcde37cee4
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-15 16:34:03 -0500

    organized middlewear to validate user input info

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,25 +3,16 @@ const router = express.Router();
 const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
+const validation = require("../middlewear/validation")
 
 //registering
 
-router.post("/register", async (req, res) => {
+router.post("/register", validation, async (req, res) => {
     try {
         //1. destructure the req.body (name, email, password)
         const { name, email, password } = req.body;
-       
-        //2. check that all fields have been filled
-        if (!name || !email || !password) {
-            return res.status(400).send(`All fields are required`);
-        }
 
-        //3. password must be at least 8 characters long
-        if (password.length < 8) {
-            return res.status(400).send(`Password must be at least 8 characters long`);
-        }
-
-        //3. check if the user exists via email
+        //2. check if the user exists via email
         const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
 
         //if user exists throw an error
@@ -29,17 +20,17 @@ router.post("/register", async (req, res) => {
             return res.status(401).send(`User already exists.`);
         }
 
-        //4. if user doesn't exist, continue with the process and crypt the password
+        //3. if user doesn't exist, continue with the process and crypt the password
         const saltRound = 10; //this makes the encryption stronger
         const salt = await bcrypt.genSalt(saltRound);
 
         const bcryptPassword = await bcrypt.hash(password, salt);
         
-        //5. enter the new user into our pernjwt database
+        //4. enter the new user into our pernjwt database
 
         const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword]);
 
-        //6. generate jwt access token;
+        //5. generate jwt access token;
 
         const accessToken = jwtGenerator(newUser.rows[0].user_id);
 
@@ -53,17 +44,12 @@ router.post("/register", async (req, res) => {
 
 //logging in
 
-router.post("/login", async (req, res) => {
+router.post("/login", validation, async (req, res) => {
     try {
         //1. destructure the req.body
         const { email, password } = req.body;
 
-        //2. check that all input fields are filled
-        if (!email || !password) {
-            return res.status(400).send(`All input fields are required`);
-        }
-
-        //3. checek that user exists
+        //2. checek that user exists
         const existingUser = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
 
         //if user does not exist, throw an error
@@ -71,7 +57,7 @@ router.post("/login", async (req, res) => {
             return res.status(401).json({ message: `Wrong email or password.` });
         }
 
-        //4. if the user exists, compare password with bcrypt password
+        //3. if the user exists, compare password with bcrypt password
         const passwordCorrect = await bcrypt.compare(password, existingUser.rows[0].user_password);
 
         if (!passwordCorrect) {
@@ -80,7 +66,7 @@ router.post("/login", async (req, res) => {
         
         console.log(passwordCorrect);
 
-        //5. if password is correct, generator access token upon sucessful login
+        //4. if password is correct, generator access token upon sucessful login
         const accessToken = jwtGenerator(existingUser.rows[0].user_id);
 
         res.json({ accessToken });
