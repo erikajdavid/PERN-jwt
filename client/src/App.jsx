7bc7e7241908d96ca6619ef35edd8fa68ad385b1
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-15 18:30:25 -0500

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
-
   return (
-    <>
-      <h1>CLIENT SIDE</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
