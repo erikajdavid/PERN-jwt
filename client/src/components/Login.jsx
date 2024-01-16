34c2ef6944c2df8690dded0077d6bbeb7768c171
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-16 00:53:20 -0500

    completed Login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = ({ setAuth }) => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+
+        try {
+            const body = {
+                email,
+                password
+            }
+
+            const response = await fetch("http://localhost:5000/auth/login", {
+                method: "POST",
+                headers: { "Content-type" : "application/json"},
+                body: JSON.stringify(body)
+            });
+
+            const parseResponse = await response.json();
+
+            console.log(parseResponse);
+
+            localStorage.setItem("token", parseResponse.accessToken);
+
+            setAuth(true);
+            
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
 
     return (
         <>
             <h1>Login</h1>
-            <button onClick={() => setAuth(true)}>Authenticate</button>
+            <form onSubmit={onSubmitForm}>
+
+                <label htmlFor="email"></label>
+                <input 
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email:"
+                    required
+                />
+
+                <label htmlFor="password"></label>
+                <input 
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password:"
+                    required
+                />
+
+                <button>Log in</button>
+            </form>
         </>
     );
 
