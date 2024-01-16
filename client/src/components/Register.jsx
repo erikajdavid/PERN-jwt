6394d4ec5ce5bfc8fef44bf01a08e998aced692f
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-16 00:56:04 -0500

    added Link to login

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Register = ({ setAuth }) => {
 
@@ -75,6 +76,7 @@ const Register = ({ setAuth }) => {
                 />
 
                 <button>Register for an account</button>
+                <Link to="/login">Login</Link>
             </form>
         </>
     );
