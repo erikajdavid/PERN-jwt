53e17dc041fb75270cb1bb9f5905472f9d3f10a0
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-15 22:03:37 -0500

    useState for authenticated user, updated routes for each path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  }
+
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login setAuth={setAuth} /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/register"
+          element={!isAuthenticated ? <Register setAuth={setAuth} /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard setAuth={setAuth} /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </>
   );
 }
 
